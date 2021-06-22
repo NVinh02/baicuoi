fa8a0f2809eb563563ba6c97601cca90a50c01fe
Vinh Duc <nguyncvinh129@gmail.com>
2021-06-22 15:41:52 +0700

    run sonarcloud

---
+++ src/com/vinh02/mathutili/Nothing.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vinh02.mathutili;
+
+/**
+ *
+ * @author Admin
+ */
+public class Nothing {
+    String my;
+    int l;
+}
